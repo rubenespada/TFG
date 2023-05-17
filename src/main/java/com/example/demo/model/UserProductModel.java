33d80af7d58a9e9ba63f73bcd96faad0fa606221
rubenespada <rubenespadav@gmail.com>
2023-05-17 20:47:48 +0200

    Relación UserProduct
    Crea las tablas, queda probar dar de alta y demás. En el controlador los métodos principales deben ser buscar por ambos id y cada uno por separado aparte del alta. La comprobación del saldo se hará desde el front.

--- src/main/java/com/example/demo/model/UserProductModel.java
+++ src/main/java/com/example/demo/model/UserProductModel.java
@@ -1,5 +1,33 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity(name = "ShopUserProduct")
+@Data
 public class UserProductModel {
+	
+	@Id
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name="idProduct" ,insertable = false, updatable=false)
+	private ProductModel product;
+	
+	@ManyToOne
+	@JoinColumn(name="idUser" ,insertable = false, updatable=false)
+	private UserModel shopUser;
+	
+	private Integer cantidad;
+	
+	private LocalDate fecha;
+	
+	private Float coste;
 
 }
