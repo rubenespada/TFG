829fbd64911cbce87eaaf124e4f4630d5c23cd11
rubenespada <rubenespadav@gmail.com>
2023-05-25 22:58:25 +0200

    AltaAccountDto
    Alta account dto actualizado

--- src/main/java/com/example/demo/dto/AltaAccountDto.java
+++ src/main/java/com/example/demo/dto/AltaAccountDto.java
@@ -1,5 +1,15 @@
 package com.example.demo.dto;
 
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
 public class AltaAccountDto {
+	
+	private Integer id;
+	private Float saldo;
+	private LocalDate fechaCad;
+	private Integer pin;
 
 }
